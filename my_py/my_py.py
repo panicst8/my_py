cacadc79a7822cc05d75f703fda00dda531a8741
Chris Milford <panicst8@yahoo.com>
2020-05-06 07:52:04 +0000

    seems right

---
+++ my_py/my_py.py
@@ -0,0 +1,11 @@
+
+class SampleClass:
+    """ Sample class to test """
+    def sample_method_add(self, first_int: int, second_int: int):
+        """ sample method to test """
+        return first_int + second_int
+
+
+def sample_func_subtract(first_int: int, second_int: int):
+    """ Sample function to test """
+    return first_int - second_int
