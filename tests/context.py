82f222c01b1ab5a0b889bafbc853cdfdd03402d1
Chris Milford <panicst8@yahoo.com>
2020-05-07 23:03:48 +0000

    fixing structure

---
+++ tests/context.py
@@ -0,0 +1,7 @@
+""" attempt to find tests """
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+import my_py  # noqa: E402 pylint: disable=unused-import
--- tests/test_my_py.py
+++ tests/test_my_py.py
@@ -1,29 +1,24 @@
 """ Sample tests using pytest and hypothesis """
-import sys
 import hypothesis.strategies as st
 from hypothesis import given
-
-sys.path.append("..")
-
-
-from my_py.my_py import SampleClass, sample_func_subtract  # noqa: E402
+from app import my_py
 
 
 def test_sample_method_add():
     """ testing for sample_method_add """
-    sample_class = SampleClass()
+    sample_class = my_py.SampleClass()
     assert sample_class.sample_method_add(2, 4) == 6  # nosec
 
 
 def test_sample_func_subtract():
     """ testing for sample_function_subtract """
-    assert sample_func_subtract(4, 3) == 1  # nosec
+    assert my_py.sample_func_subtract(4, 3) == 1  # nosec
 
 
 @given(st.integers(), st.integers())
 def test_hypothesis_test_of_calc(first_int, second_int):
     """ hypothesis test """
-    sample_class = SampleClass()
+    sample_class = my_py.SampleClass()
     assert (  # nosec
         sample_class.sample_method_add(first_int, second_int) == first_int + second_int
     )
