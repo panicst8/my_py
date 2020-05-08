01acf9307db11639336dd1bdfcc1f26588cea4e3
Chris Milford <panicst8@yahoo.com>
2020-05-08 06:36:26 +0000

    made types required

--- app/my_py.py
+++ app/my_py.py
@@ -4,11 +4,11 @@
 class SampleClass:
     """ Sample class to test """
 
-    def sample_method_add(self, first_int: int, second_int: int):
+    def sample_method_add(self, first_int: int, second_int: int) -> int:
         """ sample method to test """
         return first_int + second_int
 
 
-def sample_func_subtract(first_int: int, second_int: int):
+def sample_func_subtract(first_int: int, second_int: int) -> int:
     """ Sample function to test """
     return first_int - second_int
--- tests/test_my_py.py
+++ tests/test_my_py.py
@@ -4,19 +4,19 @@ from hypothesis import given
 from app import my_py
 
 
-def test_sample_method_add():
+def test_sample_method_add() -> None:
     """ testing for sample_method_add """
     sample_class = my_py.SampleClass()
     assert sample_class.sample_method_add(2, 4) == 6  # nosec
 
 
-def test_sample_func_subtract():
+def test_sample_func_subtract() -> None:
     """ testing for sample_function_subtract """
     assert my_py.sample_func_subtract(4, 3) == 1  # nosec
 
 
 @given(st.integers(), st.integers())
-def test_hypothesis_test_of_calc(first_int, second_int):
+def test_hypothesis_test_of_calc(first_int: int, second_int: int) -> None:
     """ hypothesis test """
     sample_class = my_py.SampleClass()
     assert (  # nosec
