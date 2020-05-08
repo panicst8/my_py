5ee106aee36946c5121d993ccf0ac8f9deedcf38
Chris Milford <panicst8@yahoo.com>
2020-05-08 04:56:57 +0000

    config

--- tests/context.py
+++
@@ -1,7 +0,0 @@
-""" attempt to find tests """
-import os
-import sys
-
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-
-import my_py  # noqa: E402 pylint: disable=unused-import
